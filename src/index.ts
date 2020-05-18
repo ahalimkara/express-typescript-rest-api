cfbd803ec6cefddd1e16d96f62f8303c71e5ef9b
Abdulhalim Kara <ahalimkara@gmail.com>
2020-05-18 11:15:37 +0100

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import * as dotenv from 'dotenv'
+import express from 'express'
+import cors from 'cors'
+import helmet from 'helmet'
+
+dotenv.config()
+
+const PORT: number = parseInt(process.env.PORT as string, 10) || 3000
+
+const app = express()
+
+app.use(helmet())
+app.use(cors())
+app.use(express.json())
+
+const server = app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`)
+})
